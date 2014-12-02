14135eab27d6df2de34a8b6100f538b5e15a5e5c
quote <quote@lv0.io>
2014-12-02 17:19:10 -0600

    add subdivide line logic. offset sub-points by normals each frame for a zig-zag pattern

--- src/main.rs
+++ src/main.rs
@@ -32,13 +32,15 @@ use event::window::{ CaptureCursor };
 struct LineVertex {
     a_pos: [f32, ..3],
     a_color: [f32, ..3],
+    a_normal: [f32, ..3],
 }
 
 impl LineVertex {
-    fn new(pos: [f32, ..3], color: [f32, ..3]) -> LineVertex {
+    fn new(pos: [f32, ..3], color: [f32, ..3], normal: [f32, ..3]) -> LineVertex {
         LineVertex {
             a_pos: pos,
             a_color: color,
+            a_normal: normal,
         }
     }
     fn rand_pos(color: [f32, ..3]) -> LineVertex {
@@ -46,13 +48,14 @@ impl LineVertex {
         let y = rand::random::<f32>();
         let z = rand::random::<f32>();
 
-        LineVertex::new([x, y, z], color)
+        LineVertex::new([x, y, z], color, [0.0, 0.0, 0.0])
     }
 }
 
 #[shader_param(LineBatch)]
 struct LineParams {
     u_model_view_proj: [[f32, ..4], ..4],
+    u_normal_mat: [[f32, ..3], ..3],
     u_alpha: f32,
 }
 
@@ -61,24 +64,31 @@ GLSL_120: b"
     #version 120
     attribute vec3 a_pos;
     attribute vec3 a_color;
+    attribute vec3 a_normal;
     varying vec4 v_color;
     uniform mat4 u_model_view_proj;
+    uniform mat3 u_normal_mat;
     uniform float u_alpha;
     void main() {
         v_color = vec4(a_color, u_alpha);
-        gl_Position = u_model_view_proj * vec4(a_pos, 1.0);
+        //gl_Position = u_model_view_proj * vec4(a_pos, 1.0);
+        //vec3 off = vec3(0.0); //normalize(u_normal_mat * a_normal);
+        gl_Position = u_model_view_proj * vec4(a_pos + a_normal * 0.02 * a_color.b, 1.0);
     }
 "
 GLSL_150: b"
     #version 150 core
     in vec3 a_pos;
     in vec3 a_color;
+    in vec3 a_normal;
     out vec4 v_color;
     uniform mat4 u_model_view_proj;
+    uniform mat3 u_normal_mat;
     uniform float u_alpha;
     void main() {
         v_color = vec4(a_color, u_alpha);
-        gl_Position = u_model_view_proj * vec4(a_pos, 1.0);
+        //vec3 off = vec3(0.0); //normalize(u_normal_mat * a_normal);
+        gl_Position = u_model_view_proj * vec4(a_pos + a_normal * 0.02 * a_color.b, 1.0);
     }
 "
 };
@@ -104,6 +114,7 @@ GLSL_150: b"
 #[inline]
 fn rand_color() -> [f32, ..3] {
     [rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()]
+    //[0.0, 0.2, 0.8]
 }
 
 fn main() {
@@ -131,10 +142,46 @@ fn main() {
 
     // start linedrawing prep
     let num_triangles = 4;
-    let num_points = num_triangles * 3;
+    let subdivide_lines = 16;
 
     // vertex data
-    let mut lines_vd = Vec::from_fn(num_points, |_| LineVertex::rand_pos(rand_color()));
+    //let mut lines_vd = Vec::from_fn(num_triangles * 3, |_| LineVertex::rand_pos(rand_color()));
+    let mut lines_vd = Vec::new(); //Vec::from_fn(num_triangles * 3, |_| LineVertex::rand_pos(rand_color()));
+    for _ in range(0, num_triangles) {
+        let a = LineVertex::rand_pos(rand_color());
+        let b = LineVertex::rand_pos(rand_color());
+        let c = LineVertex::rand_pos(rand_color());
+
+        // calculate triangle normal
+        let ba = vecmath::vec3_sub(b.a_pos, a.a_pos);
+        let ca = vecmath::vec3_sub(c.a_pos, a.a_pos);
+        let tnorm = vecmath::vec3_normalized(vecmath::vec3_cross(ba, ca));
+
+        for i in range(0u, 3) {
+            let (t1, t2) = match i {
+                    0 => (a, b),
+                    1 => (b, c),
+                    _ => (c, a),
+                };
+
+            let v = vecmath::vec3_sub(t2.a_pos, t1.a_pos);
+            let vdiv = vecmath::vec3_scale(v, 1.0 / subdivide_lines as f32);
+
+            let mut p1 = t1.a_pos;
+            lines_vd.push(LineVertex::new(p1, rand_color(), [0.0, 0.0, 0.0]));
+            p1 = vecmath::vec3_add(p1, vdiv);
+
+            for j in range(1u, subdivide_lines) {
+                if j % 2 == 0 {
+                    lines_vd.push(LineVertex::new(p1, rand_color(), tnorm));
+                } else {
+                    lines_vd.push(LineVertex::new(p1, rand_color(), vecmath::vec3_sub([0.0, 0.0, 0.0], tnorm)));
+                }
+                p1 = vecmath::vec3_add(p1, vdiv);
+            } // intentionally skip last position
+        }
+    }
+
     let lines_vd_buff = graphics.device.create_buffer::<LineVertex>(lines_vd.len(), gfx::UsageDynamic);
     graphics.device.update_buffer(lines_vd_buff, lines_vd.as_slice(), 0);
     let lines_mesh = Mesh::from_format(lines_vd_buff, lines_vd.len() as u32);
@@ -143,18 +190,18 @@ fn main() {
     let mut lines_idxd = Vec::with_capacity(num_triangles * 6);
     let mut lines_tri_idxd = Vec::with_capacity(num_triangles * 3);
     for i in range(0, num_triangles) {
-        let off = i as u8 * 3;
-        //let ioff = i * 6;
+        let off = (i * subdivide_lines) as u8 * 3;
+
         lines_idxd.push(off + 0);
-        lines_idxd.push(off + 1);
-        lines_idxd.push(off + 1);
-        lines_idxd.push(off + 2);
-        lines_idxd.push(off + 2);
+        for j in range(1, 3 * subdivide_lines as u8) {
+            lines_idxd.push(off + j);
+            lines_idxd.push(off + j);
+        }
         lines_idxd.push(off + 0);
 
         lines_tri_idxd.push(off + 0);
-        lines_tri_idxd.push(off + 1);
-        lines_tri_idxd.push(off + 2);
+        lines_tri_idxd.push(off + subdivide_lines as u8);
+        lines_tri_idxd.push(off + subdivide_lines as u8 * 2);
     }
     let lines_idx_buff = graphics.device.create_buffer::<u8>(lines_idxd.len(), gfx::UsageDynamic);
     graphics.device.update_buffer(lines_idx_buff, lines_idxd.as_slice(), 0);
@@ -173,7 +220,36 @@ fn main() {
     let mut lines_data = LineParams {
         u_model_view_proj: vecmath::mat4_id(),
         u_alpha: 1.0,
+        u_normal_mat: vecmath::mat3_id(),
     };
+
+
+
+    // // plane
+    // let plane_vd = vec!(
+    //         LineVertex::new([ 0.0 ,  0.0,  1.0], [1.0, 1.0, 1.0]), // front / nose
+    //         LineVertex::new([ 0.75,  0.0, -1.0], [1.0, 1.0, 1.0]), // left wing - 'port'
+    //         LineVertex::new([-0.75,  0.0, -1.0], [1.0, 1.0, 1.0]), // right wing - 'starboard'
+    //         LineVertex::new([ 0.0 ,  0.0, -1.0], [1.0, 1.0, 1.0]), // back midpoint between wings
+    //         LineVertex::new([ 0.0 , -0.4, -1.0], [1.0, 1.0, 1.0]), // back bottom fin
+    //     );
+
+    // let plane_idx = vec!(
+    //         0u8, 1, 3,
+    //         0, 3, 2,
+    //         0, 4, 3,
+    //         0, 3, 4,
+    //     );
+
+    // let plane_vd_b = graphics.device.create_buffer::<LineVertex>(plane_vd.len(), gfx::UsageStatic);
+    // graphics.device.update_buffer(plane_vd_b, plane_vd.as_slice(), 0);
+    // let plane_mesh = Mesh::from_format(plane_vd_b, plane_vd.len() as u32);
+
+    // let plane_idx_b = graphics.device.create_buffer::<u8>(plane_idx.len(), gfx::UsageStatic);
+    // graphics.device.update_buffer(plane_idx_b, plane_idx.as_slice(), 0);
+    // let plane_slice = plane_idx_b.to_slice(gfx::TriangleList);
+
+    // let plane_batch: LineBatch = graphics.make_batch(&lines_prog, &plane_mesh, plane_slice, &state).unwrap();
 
 
     let model = vecmath::mat4_id();
@@ -201,7 +277,6 @@ fn main() {
     let window = RefCell::new(window);
     for e in Events::new(&window) {
         use event::{RenderEvent, ResizeEvent};
-        use vecmath::col_mat4_mul;
 
         first_person.event(&e);
         e.render(|args| {
@@ -215,10 +290,10 @@ fn main() {
                 &frame
             );
 
-            let translate = vecmath::mat4_id();
+            let mut translate = vecmath::mat4_id();
 
             lines_data.u_model_view_proj = cam::model_view_projection(
-                    col_mat4_mul(model, translate),
+                    vecmath::col_mat4_mul(model, translate),
                     first_person.camera(args.ext_dt).orthogonal(),
                     projection
                 );
@@ -229,6 +304,15 @@ fn main() {
             lines_data.u_alpha = 0.3;
             graphics.draw(&lines_tri_batch, &lines_data, &frame);
 
+
+            // translate[3][0] = 3.0;
+            // lines_data.u_model_view_proj = cam::model_view_projection(
+            //         vecmath::col_mat4_mul(model, translate),
+            //         first_person.camera(args.ext_dt).orthogonal(),
+            //         projection
+            //     );
+            // graphics.draw(&plane_batch, &lines_data, &frame);
+
             graphics.end_frame();
             frame_count += 1;
 
@@ -237,7 +321,23 @@ fn main() {
             //     graphics.device.update_buffer(lines_idx_buff, lines_idxd.as_slice(), 0);
             // }
 
-            if frame_count % 20 == 0 {
+            if frame_count % 5 == 0 {
+                let color_0 = lines_vd[0].a_color;
+                // for lv in lines_vd.iter_mut() {
+                //     lv.a_normal = vecmath::vec3_sub([0.0, 0.0, 0.0], lv.a_normal);
+                // }
+                for i in range(0, lines_vd.len()-1) {
+                    lines_vd[i].a_normal = vecmath::vec3_sub([0.0, 0.0, 0.0], lines_vd[i].a_normal);
+                    lines_vd[i].a_color = lines_vd[i+1].a_color;
+                }
+                let last_idx = lines_vd.len()-1;
+                lines_vd[last_idx].a_normal = vecmath::vec3_sub([0.0, 0.0, 0.0], lines_vd[last_idx].a_normal);
+                lines_vd[last_idx].a_color = color_0;
+
+                graphics.device.update_buffer(lines_vd_buff, lines_vd.as_slice(), 0);
+            }
+
+            if false && frame_count % 20 == 0 {
                 for lv in lines_vd.iter_mut() {
                     *lv = LineVertex::rand_pos(rand_color());
                 }
@@ -257,4 +357,3 @@ fn main() {
         });
     }
 }
-
